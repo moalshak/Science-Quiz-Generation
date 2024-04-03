1ee0755ea5f51b65540f15916ae280a4d198ad3d
Roben <robendelange@gmail.com>
2024-04-03 16:10:59 +0200

    split generating questions and training options model

---
+++ generate-question-dataset.py
@@ -0,0 +1,67 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import BartForConditionalGeneration
+
+
+def train_options_generator():
+    # make sure to include cuda
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
+
+    data = load_dataset("allenai/sciq")
+
+    max_input = 512
+    max_target = 128
+    batch_size = 36
+
+    args1 = Seq2SeqTrainingArguments(
+        output_dir="./results_prediction_question",
+        evaluation_strategy='epoch',
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        gradient_accumulation_steps=2,
+        weight_decay=0.01,
+        save_total_limit=2,
+        num_train_epochs=32,
+        predict_with_generate=True,
+        eval_accumulation_steps=32,
+        fp16=torch.cuda.is_available()  # available only with CUDA
+    )
+
+    MODEL_FOLDER = "models/sciq"
+
+    questions_model = BartForConditionalGeneration.from_pretrained(f"./{MODEL_FOLDER}")
+    questions_trainer = Seq2SeqTrainer(
+        questions_model,
+        args1,
+        tokenizer=tokenizer,
+    )
+
+    def pre_process_data_question_model(data):
+        # tokenize the data
+        inputs = tokenizer(data['support'], data['correct_answer'], padding="max_length", truncation=True, max_length=max_input, return_tensors="pt")
+        targets = tokenizer(data['question'], padding="max_length", truncation=True, max_length=max_target, return_tensors="pt")
+        return {"input_ids": inputs.input_ids, "attention_mask": inputs.attention_mask, "labels": targets.input_ids}
+
+    def add_generated_question(dataset):
+        question_data = dataset.map(pre_process_data_question_model, batched=True)
+        predictions = questions_trainer.predict(question_data, max_length=64)
+        for idx in range(len(predictions)):
+            predictions[0][idx] = [token for token in predictions[0][idx] if token != -100]
+        generated_questions = tokenizer.batch_decode(predictions[0], skip_special_tokens=True)
+        return dataset.add_column("generated_question", generated_questions)
+
+    data['validation'] = add_generated_question(data['validation'])
+    data['train'] = add_generated_question(data['train'])
+    data['test'] = add_generated_question(data['test'])
+
+    print(data)
+    data.save_to_disk("datasets/generated-questions.hf")
+
+
+if __name__ == "__main__":
+    train_options_generator()
--- options-generator.py
+++ options-generator.py
@@ -1,5 +1,5 @@
 import torch
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
 from transformers import AutoTokenizer, Seq2SeqTrainingArguments, Seq2SeqTrainer
 from transformers import BartForConditionalGeneration
 
@@ -9,67 +9,17 @@ def train_options_generator():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
+    data = load_from_disk("datasets/generated-questions.hf")
+    train_data = data['train']
+    val_data = data['validation']
+    print(data)
 
     model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
     tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
 
-    data = load_dataset("allenai/sciq")
-    train_data = data['train']
-    val_data = data['validation']
-
     max_input = 512
     max_target = 128
     batch_size = 36
-
-    args1 = Seq2SeqTrainingArguments(
-        output_dir="./results_prediction_question",
-        evaluation_strategy='epoch',
-        learning_rate=2e-5,
-        per_device_train_batch_size=batch_size,
-        per_device_eval_batch_size=batch_size,
-        gradient_accumulation_steps=2,
-        weight_decay=0.01,
-        save_total_limit=2,
-        num_train_epochs=32,
-        predict_with_generate=True,
-        eval_accumulation_steps=32,
-        fp16=True  # available only with CUDA
-    )
-
-    MODEL_FOLDER = "models/sciq"
-
-    questions_model = BartForConditionalGeneration.from_pretrained(f"./{MODEL_FOLDER}")
-    questions_trainer = Seq2SeqTrainer(
-        questions_model,
-        args1,
-        tokenizer=tokenizer,
-    )
-
-    def pre_process_data_question_model(data):
-        # tokenize the data
-        inputs = tokenizer(data['support'], data['correct_answer'], padding="max_length", truncation=True, max_length=max_input, return_tensors="pt")
-        targets = tokenizer(data['question'], padding="max_length", truncation=True, max_length=max_target, return_tensors="pt")
-        return {"input_ids": inputs.input_ids, "attention_mask": inputs.attention_mask, "labels": targets.input_ids}
-
-    val_data = val_data.select(range(30))
-    question_val_data = val_data.map(pre_process_data_question_model, batched=True)
-    val_predictions = questions_trainer.predict(question_val_data, max_length=64)
-    for idx in range(len(val_predictions)):
-        val_predictions[0][idx] = [token for token in val_predictions[0][idx] if token != -100]
-    val_generated_questions = tokenizer.batch_decode(val_predictions[0], skip_special_tokens=True)
-    val_data = val_data.add_column("generated_question", val_generated_questions)
-
-    torch.cuda.empty_cache()
-
-    train_data = train_data.select(range(30))
-    question_train_data = train_data.map(pre_process_data_question_model, batched=True)
-    train_predictions = questions_trainer.predict(question_train_data, max_length=64)
-    for idx in range(len(train_predictions)):
-        train_predictions[0][idx] = [token for token in train_predictions[0][idx] if token != -100]
-    train_generated_questions = tokenizer.batch_decode(train_predictions[0], skip_special_tokens=True)
-    train_data = train_data.add_column('generated_question', train_generated_questions)
-
-    torch.cuda.empty_cache()
 
     # dataset has:
     # question, distractor3, distractor1, distractor2, correct_answer, support
@@ -88,8 +38,6 @@ def train_options_generator():
     train_data = train_data.map(pre_process_data, batched=True).shuffle(seed=42)
     val_data = val_data.map(pre_process_data, batched=True).shuffle(seed=42)
 
-    # print(tokenizer.decode(train_data['input_ids'][1], skip_special_tokens=False))
-
     model.to(device)
 
     args = Seq2SeqTrainingArguments(
@@ -104,10 +52,8 @@ def train_options_generator():
         num_train_epochs=32,
         predict_with_generate=True,
         eval_accumulation_steps=32,
-        fp16=True  # available only with CUDA
+        fp16=torch.cuda.is_available()  # available only with CUDA
     )
-
-    tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
 
     trainer = Seq2SeqTrainer(
         model,
